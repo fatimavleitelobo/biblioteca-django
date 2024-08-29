9e4ebb0dd318ad4a9571d2e552530c8ffffc462a
Fátima Vitória Leite Lôbo <fatimavleitelobo13@gmail.com>
2024-08-29 20:23:00 -0300

    :wrench: chore: alterar nome do banco de dados SQLite para 'bib.sqlite3'

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'bib.sqlite3',
     }
 }
 
