71c109d20bc50c0c2d9cffbf6eb75e8b9653f422
Fátima Vitória Leite Lôbo <fatimavleitelobo13@gmail.com>
2024-08-29 21:03:13 -0300

    :sparkles: feat(core): adicionar modelo Autor ao models.py

--- core/models.py
+++ core/models.py
@@ -10,3 +10,12 @@ class Categoria(models.Model):
     
     def __str__(self):
         return self.nome
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        return self.nome
